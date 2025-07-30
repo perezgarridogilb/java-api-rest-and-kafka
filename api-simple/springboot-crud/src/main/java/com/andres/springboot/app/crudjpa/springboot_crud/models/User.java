44fbce9e616316aed4666e249ee87de60cf4fa83
Gilberto <gilberto@teknik.mx>
2025-07-29 23:49:32 -0600

    ManyToMany Bidireccinal revisando error Bucle

--- api-simple/springboot-crud/src/main/java/com/andres/springboot/app/crudjpa/springboot_crud/models/User.java
+++ api-simple/springboot-crud/src/main/java/com/andres/springboot/app/crudjpa/springboot_crud/models/User.java
@@ -1,11 +1,13 @@
 package com.andres.springboot.app.crudjpa.springboot_crud.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 
 import org.hibernate.annotations.ManyToAny;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -41,6 +43,8 @@ public class User {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
+    // previente consulta infinita en el json
+    @JsonIgnoreProperties({"users", "handler", "hibernateLazyInitializer"})
     @ManyToMany
     @JoinTable(
         name = "users_roles",
@@ -51,9 +55,12 @@ public class User {
 
     private List<Role> roles;
 
+    public User() {
+        this.roles = new ArrayList<>();
+    }
+
     // boolean por default coloca 0
     // Boolean por default es null y coloca 1
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private boolean enabled;
 
     // es como un mutador en laravel
@@ -63,6 +70,7 @@ public class User {
     }
 
     @Transient
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private boolean admin;
 
     public Long getId() {
@@ -120,6 +128,37 @@ public class User {
 
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
     }
 
     
