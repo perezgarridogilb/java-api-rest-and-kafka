7dca78f620546e31b4508ae6ff36740f2240bc3c
Gilberto <Betho6990@gmail.com>
2025-08-03 10:57:00 -0600

    Adding roles to claims when generating the token

--- api-simple/springboot-crud/src/main/java/com/andres/springboot/app/crudjpa/springboot_crud/security/filter/JwtAuthenticationFilter.java
+++ api-simple/springboot-crud/src/main/java/com/andres/springboot/app/crudjpa/springboot_crud/security/filter/JwtAuthenticationFilter.java
@@ -1,8 +1,100 @@
 package com.andres.springboot.app.crudjpa.springboot_crud.security.filter;
 
+import java.io.IOException;
+
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.andres.springboot.app.crudjpa.springboot_crud.models.User;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.security.Key;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
+// inyeecta de forma estática como si fueran constantes de la clase
+import static com.andres.springboot.app.crudjpa.springboot_crud.security.filter.TokenJwtConfig.*;
+
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private AuthenticationManager authenticationManager;
+
+    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+
+        User user = null;
+        String username = null;
+        String password = null;
+        // TODO Auto-generated method stub
+
+        try {
+            
+            user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            username = user.getUsername();
+            password = user.getPassword();
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+        e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+                User user = (User) authResult.getPrincipal();
+                String username = user.getUsername();
+                Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+
+                Claims claims = Jwts.claims().build();
+                claims.put("authorities", roles);
+
+                String token = Jwts.builder()
+                        .subject(username)
+                        .claims(claims)
+                        .expiration(new Date(System.currentTimeMillis() + 360000000))
+                        .issuedAt(new Date())
+                        .signWith(SECRET_KEY)
+                        .compact();
+
+                response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + token);
+
+                Map<String, String> body = new HashMap<>();
+                body.put("token", token);
+                body.put("username", token);
+                body.put("message", String.format("Hole %s has iniciado sesion con exito", username));
+
+                response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+                response.setContentType("application/json");
+                response.setStatus(200);
+    }
+
+    
 }
