658658381ce40c81e2b3318a283b87bbc3fbc0ef
Gilberto <Betho6990@gmail.com>
2025-08-03 20:08:25 -0600

    Adding the JwtAuthentication Filter in the Spring Security Configuration

--- api-simple/springboot-crud/src/main/java/com/andres/springboot/app/crudjpa/springboot_crud/security/SpringSecurityConfig.java
+++ api-simple/springboot-crud/src/main/java/com/andres/springboot/app/crudjpa/springboot_crud/security/SpringSecurityConfig.java
@@ -1,10 +1,16 @@
 package com.andres.springboot.app.crudjpa.springboot_crud.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.andres.springboot.app.crudjpa.springboot_crud.security.filter.JwtAuthenticationFilter;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.http.HttpMethod;
@@ -14,6 +20,16 @@ import org.springframework.http.HttpMethod;
 
 @Configuration
 public class SpringSecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    
+    AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    } 
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -28,6 +44,7 @@ public class SpringSecurityConfig {
             .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
             .anyRequest().authenticated()
         )
+        .addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
         .sessionManagement(management -> 
             management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         )
--- api-simple/springboot-crud/src/main/java/com/andres/springboot/app/crudjpa/springboot_crud/security/filter/JwtAuthenticationFilter.java
+++ api-simple/springboot-crud/src/main/java/com/andres/springboot/app/crudjpa/springboot_crud/security/filter/JwtAuthenticationFilter.java
@@ -32,9 +32,8 @@ import static com.andres.springboot.app.crudjpa.springboot_crud.security.filter.
 
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-    private AuthenticationManager authenticationManager;
 
-    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+    private AuthenticationManager authenticationManager;
 
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
@@ -47,21 +46,21 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         User user = null;
         String username = null;
         String password = null;
-        // TODO Auto-generated method stub
 
         try {
-            
             user = new ObjectMapper().readValue(request.getInputStream(), User.class);
             username = user.getUsername();
             password = user.getPassword();
         } catch (StreamReadException e) {
             e.printStackTrace();
         } catch (DatabindException e) {
-        e.printStackTrace();
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+                password);
 
         return authenticationManager.authenticate(authenticationToken);
     }
@@ -69,32 +68,47 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
             Authentication authResult) throws IOException, ServletException {
-                User user = (User) authResult.getPrincipal();
-                String username = user.getUsername();
-                Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
 
-                Claims claims = Jwts.claims().build();
-                claims.put("authorities", roles);
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) authResult.getPrincipal();
+        String username = user.getUsername();
+        Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
 
-                String token = Jwts.builder()
-                        .subject(username)
-                        .claims(claims)
-                        .expiration(new Date(System.currentTimeMillis() + 360000000))
-                        .issuedAt(new Date())
-                        .signWith(SECRET_KEY)
-                        .compact();
+        Claims claims = Jwts.claims()
+                .add("authorities", roles)
+                .add("username", username)
+        .build();
 
-                response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + token);
 
-                Map<String, String> body = new HashMap<>();
-                body.put("token", token);
-                body.put("username", token);
-                body.put("message", String.format("Hole %s has iniciado sesion con exito", username));
+        String token = Jwts.builder()
+                .subject(username)
+                .claims(claims)
+                .expiration(new Date(System.currentTimeMillis() + 3600000))
+                .issuedAt(new Date())
+                .signWith(SECRET_KEY)
+                .compact();
 
-                response.getWriter().write(new ObjectMapper().writeValueAsString(body));
-                response.setContentType("application/json");
-                response.setStatus(200);
+        response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + token);
+
+        Map<String, String> body = new HashMap<>();
+        body.put("token", token);
+        body.put("username", username);
+        body.put("message", String.format("Hola %s has iniciado sesion con exito!", username));
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType(CONTENT_TYPE);
+        response.setStatus(200);
     }
 
-    
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException failed) throws IOException, ServletException {
+        Map<String, String> body = new HashMap<>();
+        body.put("message", "Error en la autenticacion username o password incorrectos!");
+        body.put("error", failed.getMessage());
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(401);
+        response.setContentType(CONTENT_TYPE);
+    }
+
 }
